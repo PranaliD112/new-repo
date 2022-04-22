77ecf6cf27bc82ead93c0f96773340bc088a09a8
pranali <71546716+PranaliD112@users.noreply.github.com>
2022-04-22 16:40:59 +0530

    alarm updated

--- about-us.java
+++ about-us.java
@@ -1,1 +1,6 @@
 This is 92872763637 our contact
+statistics is metric data aggreagation over the period of time
+we use alarm to automatically initiate task on our behalf
+this alarm watch single metric over the period of time and perform aone or more actions based on value of metric threshold
+this actions arenotification sent using sns topic
+we can add alarm to custom dashboard 
