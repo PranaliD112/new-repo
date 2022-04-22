d7d71ce7f0c0a6f45a149be0f5ecefde96b9eb3b
pranali <71546716+PranaliD112@users.noreply.github.com>
2022-04-22 16:56:56 +0530

    done

--- about-us.java
+++ about-us.java
@@ -4,3 +4,7 @@ we use alarm to automatically initiate task on our behalf
 this alarm watch single metric over the period of time and perform aone or more actions based on value of metric threshold
 this actions arenotification sent using sns topic
 we can add alarm to custom dashboard 
+metric can be standard or high resolution data have grabularity of 1 min and 1 sec respectiveley...when metrics are prodecd by aws services by defalut they are standard a
+and when we publish custom logs we can define them high or standard resolution if we publish them as a high then we can read and retrive logs with granularity of 1 sec or period of 5 10 30 or any mul
+tile of 60 second
+high resolution alarms allow u to react and take actions faster
